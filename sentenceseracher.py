59fbe37a62969e40891410b0063df557390011b3
Peyvand Jadidi <peyvandjadidi@gmail.com>
2021-03-05 19:20:05 +0330

    sentencesearcher.py commited

---
+++ sentenceseracher.py
@@ -0,0 +1,20 @@
+def Sentence_searcher(txt,fltr):
+    txt = txt.split('.')
+    for snt in txt :
+        if fltr.lower() in snt.lower() :
+            return snt+'.'
+    return ''
+
+res = sentence_searcher('I have a cat. I have a mat. Things are going swell.','cat')
+res2= sentence_searcher('I have a cat. I have a mat. Things are going swell.','mat')
+res3 = sentence_searcher('I have a cat. I have a mat. Things are going swell.','swell')
+print(res)
+print(res2)
+print(res3)
+print(res+res2)
+
+
+
+
+
+
